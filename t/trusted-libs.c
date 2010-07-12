b6478798435e7c54b5d8efffbc8f09558dfdb998
jake <zorgnax@gmail.com>
2010-07-11 23:48:11 -0400

    Add test for trusted-libs option.

---
+++ t/trusted-libs.c
@@ -0,0 +1,26 @@
+#include "carp.h"
+#include "tap.h"
+#include "foo.h"
+#include "bar.h"
+
+int
+main () {
+    char *expected;
+    setvbuf(stdout, NULL, _IONBF, 0);
+    plan(3);
+
+    expected = "carp from bara at foo.c line 55\n";
+    is(fooe(), expected, "carp in bar blames foo");
+    
+    carp_set("trusted-libs", "ddfoo", NULL);
+    expected = "carp from bara at trusted-libs.c line 55\n";
+#line 55
+    is(fooe(), expected, "carp in bar after trusting foo blames main");
+    
+    carp_set("suspected-libs", "ddfoo", NULL);
+    expected = "carp from bara at foo.c line 55\n";
+    is(fooe(), expected, "carp in bar blames foo after reverting trust status");
+    
+    return exit_status();
+}
+
